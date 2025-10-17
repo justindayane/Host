17a9784fad40234d4225b0de079cdc3520202204
justindayane <dayanebobo@gmail.com>
2025-10-16 20:29:15 -0400

    Initial Commit

---
+++ Host/HostApp.swift
@@ -0,0 +1,17 @@
+//
+//  HostApp.swift
+//  Host
+//
+//  Created by Justin Dayane  Gbadamassi on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct HostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
