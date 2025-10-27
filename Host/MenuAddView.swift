bf6d85faa045e724085cd8a4edcbbd2af45a65da
justindayane <dayanebobo@gmail.com>
2025-10-27 14:47:24 -0400

    First attempt to add the MenuAddView into the main menu

---
+++ Host/MenuAddView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuAddView.swift
+//  Host
+//
+//  Created by Justin Dayane  Gbadamassi on 10/27/25.
+//
+
+import SwiftUI
+
+struct MenuAddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuAddView()
+}
